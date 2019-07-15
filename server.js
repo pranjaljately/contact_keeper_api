8d3964dddbf13c005c16120ec0a02c56a80d54ae
Pranjal <>
2019-07-15 16:55:36 +0100

    Dependencies & Basic Express Server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeep Api...' })
+);
