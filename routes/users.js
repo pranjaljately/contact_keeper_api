88dc5377c91fddbdde4f5a16762bd0249a490afe
Pranjal <pjately078@gmail.com>
2019-07-17 19:12:05 +0100

    user model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const user = require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 /**
  * @route POST api/users
  * @description Register a user
  * @access Public
  */
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please enter a name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    res.send('Passed');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //Connect to DB
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
