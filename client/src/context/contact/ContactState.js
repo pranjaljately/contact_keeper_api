75d22f8c74f50538c3d52b69c6b1b448b3d4883b
Pranjal <pjately078@gmail.com>
2019-08-20 23:24:41 +0100

    Contacts context and global state

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,52 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Contact 1',
+        email: 'c1@gmail.com',
+        type: 'personal',
+      },
+      {
+        id: 1,
+        name: 'Contact 1',
+        email: 'c1@gmail.com',
+        type: 'personal',
+      },
+      {
+        id: 1,
+        name: 'Contact 1',
+        email: 'c1@gmail.com',
+        type: 'personal',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
